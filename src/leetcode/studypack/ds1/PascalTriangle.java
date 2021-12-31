2d670ad3e533d22c32f04d59202c67016b803147
Sidakdeep Singh <singhsidak54@gmail.com>
2021-12-31 22:18:45 +0530

    ds1 day 4 sols

---
+++ src/leetcode/studypack/ds1/PascalTriangle.java
@@ -0,0 +1,26 @@
+package leetcode.studypack.ds1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Problem Link - https://leetcode.com/problems/pascals-triangle/
+public class PascalTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0; i<numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for(int j=0; j<=i; j++) {
+                if(j==0 || j==i)
+                    row.add(1);
+                else
+                    row.add(res.get(i-1).get(j-1) + res.get(i-1).get(j));
+            }
+            res.add(row);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(4));
+    }
+}
---
+++ src/leetcode/studypack/ds1/ReshapeTheMatrix.java
@@ -0,0 +1,42 @@
+package leetcode.studypack.ds1;
+
+import java.util.Arrays;
+
+// Problem Link - https://leetcode.com/problems/reshape-the-matrix/
+public class ReshapeTheMatrix {
+    public static  int[][] matrixReshape(int[][] mat, int r, int c) {
+        int originalRows = mat.length;
+        int originalCols = mat[0].length;
+        if(originalRows == r && originalCols == c || originalRows * originalCols != r * c) {
+            return mat;
+        }
+
+        int[][] reshapedMatrix = new int[r][c];
+        int i = 0, j = 0, k = 0, l = 0;
+        while(k < r) {
+            l = 0;
+            while(l < c) {
+                if(j == mat[i].length) {
+                    j = 0;
+                    i++;
+                }
+                reshapedMatrix[k][l] = mat[i][j];
+                j++;
+                l++;
+            }
+            k++;
+        }
+        return reshapedMatrix;
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{1,2,3,4}};
+        int[][] ans =  matrixReshape(a, 2, 2);
+        for(int[] row : ans) {
+            for(int num : row) {
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
