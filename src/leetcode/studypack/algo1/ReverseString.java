9acbbd6a025ffaacbb731a396a2a1d040314475f
Sidakdeep Singh <singhsidak54@gmail.com>
2022-01-04 15:38:48 +0530

    algo1 day 4 sols

---
+++ src/leetcode/studypack/algo1/ReverseString.java
@@ -0,0 +1,15 @@
+package leetcode.studypack.algo1;
+
+// Problem Link - https://leetcode.com/problems/reverse-string/
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0, h = s.length - 1;
+        while(l < h) {
+            char temp = s[l];
+            s[l] = s[h];
+            s[h] = temp;
+            l++;
+            h--;
+        }
+    }
+}
---
+++ src/leetcode/studypack/algo1/ReverseWordsInAStringIII.java
@@ -0,0 +1,33 @@
+package leetcode.studypack.algo1;
+
+// Problem Link - https://leetcode.com/problems/reverse-words-in-a-string-iii/
+public class ReverseWordsInAStringIII {
+    public static void reverseWord(char[] s, int l, int h) {
+        while(l < h) {
+            char temp = s[l];
+            s[l] = s[h];
+            s[h] = temp;
+            l++;
+            h--;
+        }
+    }
+
+    public static String reverseWords(String s) {
+        char[] temp = s.toCharArray();
+        int wordStartIndex = 0;
+        for(int i=0; i< temp.length; i++) {
+            if(temp[i] == ' ') {
+                reverseWord(temp, wordStartIndex, i - 1);
+                wordStartIndex = i + 1;
+            }
+        }
+
+        reverseWord(temp, wordStartIndex, temp.length - 1);
+
+        return new String(temp);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("abc abc"));
+    }
+}
