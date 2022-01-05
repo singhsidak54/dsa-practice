7372af667efc6da9359554d2a0f2d6bce733eafa
Sidakdeep Singh <singhsidak54@gmail.com>
2022-01-05 12:51:46 +0530

    add ds1 day 9 sols

---
+++ src/leetcode/studypack/ds1/ImplementQueueUsingStacks.java
@@ -0,0 +1,51 @@
+package leetcode.studypack.ds1;
+
+import java.util.Stack;
+
+// Problem Link - https://leetcode.com/problems/implement-queue-using-stacks/
+public class ImplementQueueUsingStacks {
+    static class MyQueue {
+        private Stack<Integer> s1, s2;
+        private int s1Front = -1;
+        public MyQueue() {
+            s1 = new Stack<>();
+            s2 = new Stack<>();
+        }
+
+        public void push(int x) {
+            if(s1.isEmpty())
+                s1Front = x;
+            s1.push(x);
+        }
+
+        public int pop() {
+            if(s2.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+            return s2.pop();
+        }
+
+        public int peek() {
+            if(s2.isEmpty()) {
+                return s1Front;
+            }
+            return s2.peek();
+        }
+
+        public boolean empty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue dummy = new MyQueue();
+        dummy.push(1);
+        dummy.push(2);
+        dummy.pop();
+        dummy.push(4);
+        while(!dummy.empty())
+            System.out.println(dummy.pop());
+    }
+}
---
+++ src/leetcode/studypack/ds1/ValidParentheses.java
@@ -0,0 +1,29 @@
+package leetcode.studypack.ds1;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+// Problem Link - https://leetcode.com/problems/valid-parentheses/
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else if (c == ')' || c == '}' || c == ']') {
+                if(stack.isEmpty() || stack.pop() != map.get(c))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[{}]";
+        System.out.println(isValid(s));
+    }
+}
