50aa4ca9582e8939ec13055e01affd2f82e0f820
singhsidak54 <singhsidak54@gmail.com>
2022-06-14 01:22:48 +0530

    add daily - june day 13 sol

---
+++ src/leetcode/studypack/daily/Triangle.java
@@ -0,0 +1,20 @@
+package daily;
+
+import java.util.List;
+
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        List<Integer> dp = triangle.get(n-1);
+        for(int i=n-2; i >=0; i--) {
+            List<Integer> currentList = triangle.get(i);
+            for(int j=0; j<currentList.size(); j++) {
+                int min = Math.min(dp.get(j), dp.get(j+1));
+                currentList.set(j, currentList.get(j) + min);
+            }
+            dp = currentList;
+        }
+
+        return dp.get(0);
+    }
+}
