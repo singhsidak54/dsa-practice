f9f90694a33dea531bb77606b663b36d4f331ba5
singhsidak54 <singhsidak54@gmail.com>
2022-07-12 23:50:44 +0530

    add bs2 day 2 sols

---
+++ src/leetcode/studypack/bs2/FindKClosestElements.java
@@ -0,0 +1,62 @@
+package bs2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FindKClosestElements {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int start = 0, end = arr.length - k, mid;
+        while (start < end) {
+            mid = start + (end - start) / 2;
+            if (x > (arr[mid] + (arr[mid + k] - arr[mid]) / 2)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return Arrays.stream(arr, start, start + k).boxed().collect(Collectors.toList());
+    }
+
+    public static int findClosestElement(int[] arr, int x) {
+        if(arr.length == 1) return 0;
+        int start = 0, end = arr.length - 1, mid;
+        while (end - start != 1) {
+            mid = start + (end - start) / 2;
+            if(arr[mid] == x) {
+                return mid;
+            } else if(arr[mid] > x) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        return Math.abs(arr[start] - x) <= Math.abs(arr[end] - x) ? start : end;
+    }
+    public static List<Integer> findClosestElements2(int[] arr, int k, int x) {
+        int closestElementIndex = findClosestElement(arr, x);
+        int start = closestElementIndex, end = closestElementIndex;
+        int nCloser = 1;
+        while(k > nCloser) {
+            if(start - 1 >= 0 && end + 1 < arr.length) {
+                if(Math.abs(arr[start - 1] - x) <= Math.abs(arr[end + 1] - x)) {
+                    start--;
+                } else {
+                    end++;
+                }
+            } else if (start - 1 >= 0) {
+                start -= k - nCloser;
+                break;
+            }
+            nCloser++;
+        }
+
+        return Arrays.stream(arr, start, start + k).boxed().collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findClosestElements2(new int[] {1}, 1, 0));
+    }
+}
---
+++ src/leetcode/studypack/bs2/FindTheStudentThatWillReplaceTheChalk.java
@@ -0,0 +1,29 @@
+package bs2;
+
+public class FindTheStudentThatWillReplaceTheChalk {
+    public int chalkReplacer(int[] chalk, int k) {
+        if(chalk.length == 1) return 0;
+
+        int n = chalk.length;
+        long[] temp = new long[n];
+        temp[0] = chalk[0];
+        for(int i=1; i<n; i++) {
+            temp[i] = temp[i - 1] + chalk[i];
+        }
+
+        long t = k;
+        t = t % temp[n - 1];
+
+        int l = 0, h = n - 1, m;
+        while (l < h) {
+            m = l + (h-l)/2;
+            if(temp[m] < t) {
+                l = m + 1;
+            } else {
+                h = m;
+            }
+        }
+
+        return l;
+    }
+}
