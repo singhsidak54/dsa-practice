acadc8893e6dc0c07c15bbc89d21a35c13be553c
singhsidak54 <singhsidak54@gmail.com>
2022-07-14 00:10:08 +0530

    add LC75 L1 day 7 sols

---
+++ src/leetcode/studypack/lc75l1/FirstBadVersion.java
@@ -0,0 +1,21 @@
+package lc75l1;
+
+public class FirstBadVersion {
+    public static boolean isBadVersion(int n) {
+        return false;
+    }
+
+    public int firstBadVersion(int n) {
+        int low = 1, high = n, mid;
+        while(low < high) {
+            mid = low + (high - low)/2;
+            if(isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
