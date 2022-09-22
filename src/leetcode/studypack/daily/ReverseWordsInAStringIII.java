a6efc272a3dbb15e7e327b2485dd98c66067707d
singhsidak54 <singhsidak54@gmail.com>
2022-09-22 18:48:30 +0530

    add daily sol 22/09/22

---
+++ src/leetcode/studypack/daily/ReverseWordsInAStringIII.java
@@ -0,0 +1,28 @@
+package daily;
+
+public class ReverseWordsInAStringIII {
+    public void reverseWord(char[] sArray, int l, int h) {
+        while(l < h) {
+            char temp = sArray[l];
+            sArray[l] = sArray[h];
+            sArray[h] = temp;
+            l++;
+            h--;
+        }
+    }
+
+    public String reverseWords(String s) {
+        char[] sArray = s.toCharArray();
+        int start = 0, i = 0;
+        while (i < s.length()) {
+            if(Character.isWhitespace(sArray[i])) {
+                reverseWord(sArray, start, i);
+                start = i + 1;
+            }
+            i++;
+        }
+
+        reverseWord(sArray, start, i-1);
+        return new String(sArray);
+    }
+}
