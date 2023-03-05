589c3faeff019a1ec5bdc33912f368b709f5663b
singhsidak54 <singhsidak54@gmail.com>
2023-03-05 17:38:07 +0530

    update bloomberg sols

---
+++ src/leetcode/companies/Bloomberg/DesignMostRecentlyUsedQueue.java
@@ -0,0 +1,14 @@
+package companies.Bloomberg;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class DesignMostRecentlyUsedQueue {
+    TreeSet<Integer> treeSet;
+    HashMap<Integer, Integer> freqCount;
+    public DesignMostRecentlyUsedQueue(int n) {
+        treeSet = new TreeSet<>();
+    }
+
+
+}
